4c5414e164f9c067a84230cf253ff9b27e815f5f
a1a_100006987931_JingxianZhang <a1a_100006987931_JingxianZhang@git.cloud.tencent.com>
2018-11-01 23:25:51 +0800

    miteams project

---
+++ teams-startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./dev/newserver/
+screen -dmS web8001 python server.py --port=8001
+screen -dmS web8002 python server.py --port=8002
+screen -dmS fetcher java -jar fetcher.jar 100
+screen -dmS web8001 python outlookfetcher.py --port=8001
+screen -dmS web8002 python outlookfetcher.py --port=8002
+
